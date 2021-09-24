54cc0aecdb2f8562c068749be8c3e3137a2f2b72
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-24 14:13:06 -0400

    commit tree 12900278ba9ca087ddd9b094aaf59c08987bde8f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 25
+let num4 = 7 
+let mod = num3%num4 
+mod; 
+
+
+let max = Math.max(1, 4, 8, 20) 
